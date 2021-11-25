97045d22304f8e8472d317ab5b9fa996b045243c
Jeferson Medeiros <jeferson.medeiros@portotech.org>
2021-11-25 08:50:31 -0300

    add student domain and rabbitconfig

---
+++ src/main/java/com/senai/sender/rabbitmq/RabbitConfig.java
@@ -0,0 +1,7 @@
+package com.senai.sender.rabbitmq;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+}
---
+++ src/main/java/com/senai/sender/rabbitmq/RabbitSender.java
@@ -0,0 +1,7 @@
+package com.senai.sender.rabbitmq;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitSender {
+}
